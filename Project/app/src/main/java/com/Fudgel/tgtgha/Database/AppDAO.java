bee4a91bfe23b19fe0aea96a9c020e572fd0e941
Rune Bjørn Lassen <Odenmafia@hotmail.com>
2019-05-07 15:45:38 +0200

    Libraries updated and Login via Google works.

--- Project/app/src/main/java/com/Fudgel/tgtgha/Database/AppDAO.java
+++ Project/app/src/main/java/com/Fudgel/tgtgha/Database/AppDAO.java
@@ -1,10 +1,10 @@
 package com.Fudgel.tgtgha.Database;
 
-import android.arch.persistence.room.Delete;
-import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.OnConflictStrategy;
-import android.arch.persistence.room.Query;
-import android.arch.persistence.room.Update;
+//import android.arch.persistence.room.Delete;
+//import android.arch.persistence.room.Insert;
+//import android.arch.persistence.room.OnConflictStrategy;
+//import android.arch.persistence.room.Query;
+//import android.arch.persistence.room.Update;
 
 import java.util.List;
 
