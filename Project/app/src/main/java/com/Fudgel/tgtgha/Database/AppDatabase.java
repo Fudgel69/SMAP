057742795040732b5833ad3a2bb55c3f9ac09d26
Rune Bjørn <OdenseMafia@Hotmail.com>
2019-05-19 22:33:18 +0200

    Removed useless classes activities and code

--- Project/app/src/main/java/com/Fudgel/tgtgha/Database/AppDatabase.java
+++
@@ -1,8 +0,0 @@
-//package com.Fudgel.tgtgha.Database;
-//
-//        import android.arch.persistence.room.RoomDatabase;
-//
-//public abstract class AppDatabase extends RoomDatabase {
-//
-//    public abstract AppDAO appDAO();
-//}
